1c94f733c4309892c7671099ef9e4a0f191228a4
Kasnix <luc.kasnix@gmail.com>
2026-05-02 18:05:25 -0300

    chore: specify a version for Node

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vite.dev/config/
-export default defineConfig(async () => ({
+const viteConfig = defineConfig(async () => ({
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
-  // 1. prevent Vite from obscuring rust errors
+  // Prevent Vite from obscuring Rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+  // Tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
@@ -22,8 +20,10 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
+      // Tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
 }));
+
+export default viteConfig;
